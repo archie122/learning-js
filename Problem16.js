97d2e0226e972cc0333ebee5ca07232103fa4281
Archie <110494298+archie122@users.noreply.github.com>
2023-06-07 22:26:23 -0600

    Create Problem16.js

---
+++ Problem16.js
@@ -0,0 +1,10 @@
+var buildArray = function(nums) {
+    var result = [];
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = nums[nums[i]]
+    }
+
+    return result;
+};
+
+console.log(buildArray([0,2,1,5,3,4]))
